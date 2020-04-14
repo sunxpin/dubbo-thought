b25a2cb4831ad04e27c4e74e749ac0db50b32265
xpinsun <sxp141747@163.com>
2020-04-14 09:37:39 +0800

    dubbo-thought init

---
+++ base-spi/src/main/java/com/enjoy/infoService/impl/InfoServiceCImpl.java
@@ -0,0 +1,24 @@
+package com.enjoy.infoService.impl;
+
+
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.common.extension.Adaptive;
+import com.enjoy.service.InfoService;
+
+//@Adaptive
+public class InfoServiceCImpl implements InfoService {
+
+    @Override
+    public Object sayHello(String name) {
+        System.out.println(name+",你好，调通了C实现！");
+        return name+",你好，调通了C实现！";
+    }
+
+    @Override
+    public Object passInfo(String msg, URL url) {
+        System.out.println("恭喜你，调通了C实现");
+        return msg;
+    }
+
+}
